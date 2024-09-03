e747e0ba8f9a57962622412f3dd82b8f2be6e2c3
leftycurry <xuyichenghk@gmail.com>
2024-09-03 20:04:00 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
-print("Hello, world!")
+def greet_user(name):
+            return f"Hello, {name}!"
+
+        def run_python():
+            name_input = Element('variableName').element.value
+            result = greet_user(name_input)
+            Element('result').element.innerText = result
+
+        Element('runPythonButton').element.onclick = run_python
