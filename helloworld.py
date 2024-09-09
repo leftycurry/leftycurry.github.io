9793abb8e693a943ad0042878bc51ed5ec59dd6c
leftycurry <xuyichenghk@gmail.com>
2024-09-10 04:05:20 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -8,31 +8,3 @@ def add(a, b):
 def eoqtry(D, S, C, I):
     return D+S+C+I
 
-
-def eoq(D, S, C, I):
-    return
-        numerator = 2 * D * S
-        denominator = I * C
-        
-        if denominator == 0:
-            print("The denominator is zero. Please check your inputs for C and I.")
-            return
-        
-        Q_squared = numerator / denominator
-        
-        if Q_squared < 0:
-            print("There is no real solution for Q. The value is negative. please check your inputs again")
-            return
-        
-        Q = math.sqrt(Q_squared)
-        
-        # Round up
-        Q_up = math.ceil(Q)
-        
-        # Calculate Total Cost (TC)
-        TC = (D / Q_up) * S + (I * C / 2) * Q_up
-        
-        # Conclusions
-        print(f"Your economic order quantity will be: {Q_up}")
-        print(f"Your total inventory cost will be: {TC:.2f}")
-    #return (2 * D * S) / (I * C)
