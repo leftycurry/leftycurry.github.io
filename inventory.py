1a796793bb699121b655c2d4e9ef2701110e6ae4
leftycurry <xuyichenghk@gmail.com>
2024-09-11 13:47:22 +0800

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -32,27 +32,5 @@ def eoq(D, S, C, I):
     except ValueError:
         print("Invalid input. Please enter numeric values.")
         
-import math
-
-def instant(D, S, C, I, LT):
-    try:
-        # Calculate total cost and economic order quantity 
-        numerator = 2 * D * S
-        denominator = I * C
-        Q_squared = numerator / denominator
-        Q = math.sqrt(Q_squared)
-        
-        # Round up
-        Q_up = math.ceil(Q)
-        TC = (D / Q_up) * S + (I * C / 2) * Q_up
-
-        # Calculate Reorder point quantity in units 
-        R_square = (D / 52) * LT
-        R_up = math.ceil(R_square)
-
-        # Conclusion 
-        message = f"When the inventory level drops to {R_up} units, place a replenishment order for {Q_up}."
-        print(message)
-        return message
 
 
